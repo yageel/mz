c797e06dd7dea4e913b441c6bc9c23a5f62586bd
leshengchang <leshengchang@millionmake.com>
2017-06-01 17:59:15 +0800

    ds

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -35,6 +35,7 @@ class UserController extends BaseController {
         // 进行分页数据查询 注意limit方法的参数要使用Page类的属性
         $list = $db->where($where)->order("id DESC")->limit($Page->firstRow . ',' . $Page->listRows)->select();
 
+        $this->assign('total_pages', $Page->totalPages);
         $this->assign('page', $show);
         $this->assign('list', $list);
         $this->display();
@@ -44,7 +45,21 @@ class UserController extends BaseController {
      * 提现流水
      */
     public function cash_record(){
+        $where = [];
+        $where['openid'] = $this->openid;
 
+        $db = M('cash_record'); // 实例化User对象
+        $count = $db->where($where)->count();// 查询满足要求的总记录数
+        $Page = new Page($count, 20);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+
+        $show = $Page->show();// 分页显示输出
+        // 进行分页数据查询 注意limit方法的参数要使用Page类的属性
+        $list = $db->where($where)->order("id DESC")->limit($Page->firstRow . ',' . $Page->listRows)->select();
+
+        $this->assign('total_pages', $Page->totalPages);
+        $this->assign('page', $show);
+        $this->assign('list', $list);
+        $this->display();
     }
 
     /**
