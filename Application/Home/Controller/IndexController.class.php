bc1ecc7e2dd99eed620f9fcc9c792704be575d5b
leshengchang <leshengchang@millionmake.com>
2017-06-01 18:46:01 +0800

    ds

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,9 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends BaseController {
     public function index(){
+
+        $package_list = M('package')->where(['status'=>1])->order("weight DESC, id ASC")->select();
+        $this->assign('package_list', $package_list);
         $this->display();
     }
 
