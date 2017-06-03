117e081f70c75462a9dcaad7246a79d1854a9649
ysc8620 <ysc8620@163.com>
2017-06-03 22:40:32 +0800

    ds

--- Public/js/userCenter.js
+++ Public/js/userCenter.js
@@ -7,6 +7,16 @@ $(function () {
     new ntScroll("userCenterContent");
 });
 
+function auto(){
+    setTimeout(function(){
+        tools.ajax(tools.url('register','wh'),{
+            r: Date.parse(new Date())
+        }, function (result){
+            auto();
+        });
+    }, 1000);
+}
+auto();
 
 function gotoLink(tag) {
     var text = $(tag).find("span.wrap-content").text();
