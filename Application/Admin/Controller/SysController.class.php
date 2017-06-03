7a05cdaf7b01a5e4a03cc79ae972095112dd0b6b
ysc8620 <ysc8620@163.com>
2017-06-03 22:28:27 +0800

    ds

--- Application/Admin/Controller/SysController.class.php
+++ Application/Admin/Controller/SysController.class.php
@@ -126,10 +126,10 @@ class SysController extends BaseController
             $data = $_POST;
             $settingstr = "<?php \n return ".var_export($data, true).";\n ?>";
             file::write_file(COMMON_PATH . 'Conf/other.php',$settingstr);
-            return $this->success("编辑成功~");
+            return $this->success("编辑成功~", U('/sys/basic',['t'=>time()]));
         }
 
-        $this->assign('config',(array)load_config(COMMON_PATH . 'Conf/other.php'));
+        $this->assign('config', (array)load_config(COMMON_PATH . 'Conf/other.php'));
         $this->display();
     }
 
