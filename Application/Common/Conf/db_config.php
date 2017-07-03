8fa39e322bbbbc8cb4749370d2a8408b24cf8cf8
ysc8620 <ysc8620@163.com>
2017-07-04 06:27:39 +0800

    ds

--- Application/Common/Conf/db_config.php
+++ Application/Common/Conf/db_config.php
@@ -5,8 +5,8 @@ return array (
     'DB_CHARSET'=> 'utf8mb4',
     'DB_HOST' => '127.0.0.1',
     'DB_NAME' => 'mz',
-    'DB_USER' => 'root',
-    'DB_PWD' => '123456',
+    'DB_USER' => 'qd',
+    'DB_PWD' => 'Qd888',
     //'DB_RW_SEPARATE'=>true,
     'DB_PREFIX' => 't_',
     'DB_PORT' => '3306', );
